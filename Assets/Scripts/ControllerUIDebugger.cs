0b2605627ad202374bfb6f211c43e10ab1d8cc9f
Justin <RhinoBabies@users.noreply.github.com>
2017-10-05 00:12:02 -0700

    Control additions
    Changed camera view to be a little more distant; UI sprites added to debugger (some scripts added). Joysticks 4 and 5 need to be added to Input Manager for Mouse X and Mouse Y to implement gamepad camera control.

---
+++ Assets/Scripts/ControllerUIDebugger.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerUIDebugger : MonoBehaviour {
+
+	public GameObject upButton;
+	public GameObject downButton;
+	public GameObject rightButton;
+	public GameObject leftButton;
+
+	void Update()
+	{
+		if (Input.GetButton("Jump"))
+		{
+			Debug.Log ("Jumping!");
+			upButton.SetActive (true);
+		}
+		else
+		{	
+			Debug.Log ("Not jumping!");
+			upButton.SetActive (false);
+		}
+
+		if (Input.GetButton("Fire1"))
+		{
+			Debug.Log ("Fire1!");
+			downButton.SetActive (true);
+		}
+		else
+		{	
+			Debug.Log ("No Fire1!");
+			downButton.SetActive (false);
+		}
+
+		if (Input.GetButton("Fire3"))
+		{
+			Debug.Log ("Fire3!");
+			rightButton.SetActive (true);
+		}
+		else
+		{	
+			Debug.Log ("No Fire3!");
+			rightButton.SetActive (false);
+		}
+
+		if (Input.GetButton("Cancel"))
+		{
+			Debug.Log ("Cancel!");
+			leftButton.SetActive (true);
+		}
+		else
+		{	
+			Debug.Log ("Not cancelling!");
+			leftButton.SetActive (false);
+		}
+	}
+}
