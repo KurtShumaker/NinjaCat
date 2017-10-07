20fdbdbf91c2337fe31ffdbabab9884d7b8077a5
Justin <RhinoBabies@users.noreply.github.com>
2017-10-06 18:46:29 -0700

    Input changes, crouch toggles
    Changed keyboard input keys for Walk, Crouch, and Block. Crouch toggles now (but finicky)!

--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/ThirdPersonUserControl.cs
@@ -12,7 +12,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private Vector3 m_CamForward;             // The current forward direction of the camera
         private Vector3 m_Move;
         private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
-
+		private bool crouch;
         
         private void Start()
         {
@@ -48,7 +48,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             // read inputs
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
-			bool crouch = CrossPlatformInputManager.GetButton("Crouch");
+			if (CrossPlatformInputManager.GetButtonDown("Crouch"))
+				crouch = !crouch;
 
             // calculate move direction to pass to character
             if (m_Cam != null)
@@ -64,7 +65,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             }
 #if !MOBILE_INPUT
 			// walk speed multiplier
-	        if (Input.GetKey(KeyCode.LeftShift)) m_Move *= 0.5f;
+			if (Input.GetKey(KeyCode.LeftAlt)) m_Move *= 0.5f;
 #endif
 
             // pass all parameters to the character control script
