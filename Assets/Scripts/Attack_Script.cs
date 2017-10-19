66c92f7abc9431163516feaf031f9b737e2278e2
Justin <RhinoBabies@users.noreply.github.com>
2017-10-18 23:06:47 -0700

    Billboard Enemy's HP
    Billboard EnemyHealth upon taking damage.

--- Assets/Scripts/Attack_Script.cs
+++ Assets/Scripts/Attack_Script.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class Attack_Script : MonoBehaviour {
 
-    public int m_damagePerHit;
-
+	//Attack_Script could be modified and attached to individual weapons; when a weapon impacts an enemy, do damage
+    public int m_damagePerHit; //could be changed to become weapon specific
 
     private void OnCollisionStay(Collision collision)
     {
-        if (collision.rigidbody)
+        if (collision.rigidbody) //if the target of the collision has a rigidbody, try to do damage to it
         {
+			Debug.Log ("Colliding with rigidbody");
             EnemyHealth enemyHealth = collision.collider.GetComponent<EnemyHealth>();   // checks to see if the object attacked has a EnemyHealth script
             enemyHealth.TakeDamage(m_damagePerHit);
         }
     }
- 
 }
--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,26 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour {
 
     public int startingHealth;
     public int currentHealth;
-    
+	public Text HealthCounter;
 
     private void Awake()
     {
         currentHealth = startingHealth;
-
-    }
-
+		HealthCounter.text = currentHealth.ToString();
+		HealthCounter.gameObject.SetActive (false);
+	}
    
     public void TakeDamage (int amount)
     {
         currentHealth -= amount;
-        if (currentHealth < 1)
-            Destroy(gameObject, 0.1f);
-   
         
-    }
+		HealthCounter.gameObject.SetActive (true); //show UI text bubble
+		HealthCounter.text = currentHealth.ToString(); //update text value with currentHealth
+
+		if (currentHealth < 1) {
+			Destroy (gameObject, 0.1f);
+			HealthCounter.gameObject.SetActive (false); //if dead, don't show health anymore
+		}
+	}
 }
