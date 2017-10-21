f87c63c0b678c1b5df914ae63c2be70339895eb9
Kurt Shumaker <kshumak@gmail.com>
2017-10-21 14:18:39 -0700

    Added more time functions
    UGen.cs:
    -Added NORMAL_SPEED constant.
    -Added currentSpeed static property.
    -Changed unpause() to resume(). it now returns to the speed it had before game was paused.
    -Added normalSpeed(). Returns game to NORMAL_SPEED.
    -Added setSpeed(float). Sets game to given speed. Does not unpause if game is paused.
    
    UChar.cs:
    -Fixed some comment errors.
    -Made ConeLOS public and fixed a bug related to the angle.

--- Assets/Scripts/Utility/UChar.cs
+++ Assets/Scripts/Utility/UChar.cs
@@ -24,39 +24,11 @@ namespace Ninjacat.Utility {
         /// <returns>The score</returns>
         public delegate float ScoreFunc(float angle, float distance);
 
+
+
         // ===========================
         // * PRIVATE UTILITY METHODS *
         // ===========================
-
-        /// <summary>
-        /// Try to hit nearby object in a cone in front of character. Need LOS.
-        /// </summary>
-        /// <param name="charTrans">Transform information of the character making the hit.</param>
-        /// <param name="obj">Object of the hit attempt.</param>
-        /// <param name="angle">Angle of cone.</param>
-        /// <param name="distance">The maximum reach of the hit attempt.</param>
-        /// <returns>True if object is hit, else false.</returns>
-        static private bool coneLOS(Transform charTrans, Collider obj, float angle, float distance) {
-            RaycastHit hit;
-
-            // direction toward object
-            Vector3 rayDirection = obj.transform.position - charTrans.position;
-
-            // check if object falls within the cone's direction
-            if (Vector3.Angle(rayDirection, charTrans.forward) < angle) {
-                // detect if object is within distance
-                if (Physics.Raycast(charTrans.position, rayDirection, out hit, distance)) {
-                    // if it hit the object you were looking for
-                    if (hit.collider.GetInstanceID() == obj.GetInstanceID()) 
-                        return true;
-                }
-            }
-
-            // object was not hit
-            return false;
-        }
-
-
 
         /// <summary>
         /// Calculates a score for the likelihood an object was the target based purely on aim.
@@ -76,6 +48,39 @@ namespace Ninjacat.Utility {
         // ==========================
 
         /// <summary>
+        /// Try to hit nearby object in a cone in front of character. Need LOS.
+        /// </summary>
+        /// <param name="charTrans">Transform information of the character making the hit.</param>
+        /// <param name="obj">Object of the hit attempt.</param>
+        /// <param name="angle">Angle of cone.</param>
+        /// <param name="distance">The maximum reach of the hit attempt.</param>
+        /// <returns>True if object is hit, else false.</returns>
+        static public bool coneLOS(Transform charTrans, Collider obj, float angle, float distance)
+        {
+            RaycastHit hit;
+
+            // direction toward object
+            Vector3 rayDirection = obj.transform.position - charTrans.position;
+
+            // check if object falls within the cone's direction
+            if (Vector3.Angle(rayDirection, charTrans.forward) <= angle)
+            {
+                // detect if object is within distance and LOS
+                if (Physics.Raycast(charTrans.position, rayDirection, out hit, distance))
+                {
+                    // if it hit the object you were looking for
+                    if (hit.collider.GetInstanceID() == obj.GetInstanceID())
+                        return true;
+                }
+            }
+
+            // object was not hit
+            return false;
+        }
+
+
+
+        /// <summary>
         /// Interact with nearby object of given type (layer).
         /// </summary>
         /// <param name="charTrans">Transform information of the character attempting the interaction.</param>
@@ -93,7 +98,7 @@ namespace Ninjacat.Utility {
             // Get list of nearby objects in desired layers
             Collider[] objects = Physics.OverlapSphere(charTrans.position, distance, layerMask);
 
-            // get the the likeliest target object
+            // get the likeliest target object
             bestScore = calcScore(angle + 1.0f, distance + 1.0f); // initialize bestScore
             foreach (Collider obj in objects) { // check each nearby object
                 if (coneLOS(charTrans, obj, angle, distance)) { // if object was hit, check its target score
--- Assets/Scripts/Utility/UGen.cs
+++ Assets/Scripts/Utility/UGen.cs
@@ -9,6 +9,22 @@ namespace Ninjacat.Utility
     /// </summary>
     static public class UGen
     {
+        // =============
+        // * CONSTANTS *
+        // =============
+
+        private const float NORMAL_SPEED = 1.0f;
+
+        // ==============
+        // * PROPERTIES *
+        // ==============
+
+        static private float currentSpeed = NORMAL_SPEED;
+        
+        // =========================================
+        // * PUBLIC METHODS FOR CHANGING GAMESPEED *
+        // =========================================
+
         /// <summary>
         /// Pauses the game by setting the timescale equal to zero.
         /// </summary>
@@ -19,11 +35,30 @@ namespace Ninjacat.Utility
         }
 
         /// <summary>
-        /// Unpauses the game by setting the timescale back to normal.
+        /// Resumes the game by setting the timescale back to normal.
         /// </summary>
-        static public void unpause()
+        static public void resume()
         {
-            Time.timeScale = 1.0f;
+            Time.timeScale = currentSpeed;
+        }
+
+        /// <summary>
+        /// Returns the timescale to the normal game speed.
+        /// </summary>
+        static public void normalSpeed() {
+            Time.timeScale = NORMAL_SPEED;
+            currentSpeed = NORMAL_SPEED;
+        }
+
+        /// <summary>
+        /// Sets the timescale to the given speed. Does not resume if game is paused.
+        /// </summary>
+        /// <param name="speed">Value to set timescale.</param>
+        static public void setSpeed(float speed) {
+            if (Time.timeScale != 0.0f)
+                Time.timeScale = speed;
+
+            currentSpeed = speed;
         }
     }
 }
