6f00e85fe7d9c94bdba94317ebb84e48d7a8472f
Justin <RhinoBabies@users.noreply.github.com>
2017-10-22 21:10:24 -0700

    Revert "Merge branch 'combatScripts' into attack-animation"
    This reverts commit 88c6f6575b43c9951c29a6ed17ebd0491c9a577e, reversing
    changes made to d9299aace16ac192946f0bddf22edfb7be70c63c.

--- Assets/Scripts/UI Debugger/ControllerUIDebugger.cs
+++ Assets/Scripts/UI Debugger/ControllerUIDebugger.cs
@@ -13,37 +13,45 @@ public class ControllerUIDebugger : MonoBehaviour {
 	{
 		if (Input.GetButton("Interact"))
 		{
+			Debug.Log ("Interact!");
 			upButton.SetActive (true);
 		}
 		else
 		{	
+			Debug.Log ("Not Interacting!");
 			upButton.SetActive (false);
 		}
 
 		if (Input.GetButton("Jump"))
 		{
+			Debug.Log ("Jumping!");
 			downButton.SetActive (true);
 		}
 		else
 		{	
+			Debug.Log ("No jumping!");
 			downButton.SetActive (false);
 		}
 
 		if (Input.GetButton("Crouch"))
 		{
+			Debug.Log ("Crouch!");
 			rightButton.SetActive (true);
 		}
 		else
 		{	
+			Debug.Log ("No crouch!");
 			rightButton.SetActive (false);
 		}
 
 		if (Input.GetButton("Dodge"))
 		{
+			Debug.Log ("Dodge!");
 			leftButton.SetActive (true);
 		}
 		else
 		{	
+			Debug.Log ("Not dodging!");
 			leftButton.SetActive (false);
 		}
 	}
--- Assets/Scripts/UI Debugger/ShoulderButtonsUIDebugger.cs
+++ Assets/Scripts/UI Debugger/ShoulderButtonsUIDebugger.cs
@@ -13,37 +13,45 @@ public class ShoulderButtonsUIDebugger : MonoBehaviour {
 	{
 		if (Input.GetButton("Fire1"))
 		{
+			Debug.Log ("Attack!");
 			rightBumper.SetActive (true);
 		}
 		else
 		{	
+			Debug.Log ("Not attacking!");
 			rightBumper.SetActive (false);
 		}
 
 		if (Input.GetButton("QuickMenu"))
 		{
+			Debug.Log ("Quick menu!");
 			leftBumper.SetActive (true);
 		}
 		else
 		{	
+			Debug.Log ("No quick menu!");
 			leftBumper.SetActive (false);
 		}
 
 		if (Input.GetAxis("Fire2") > 0f || Input.GetButton("Fire2"))
 		{
+			Debug.Log ("Secondary Attack!");
 			rightTrigger.SetActive (true);
 		}
 		else
 		{	
+			Debug.Log ("No secondary attack!");
 			rightTrigger.SetActive (false);
 		}
 
 		if (Input.GetAxis("Block") > 0f || Input.GetButton("Block"))
 		{
+			Debug.Log ("Block!");
 			leftTrigger.SetActive (true);
 		}
 		else
 		{	
+			Debug.Log ("Not blocking!");
 			leftTrigger.SetActive (false);
 		}
 	}
