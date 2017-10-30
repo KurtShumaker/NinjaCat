08138c0d6eba2fe57a4c2cb3b48f24a875a1fd7b
Justin <vessdawg@gmail.com>
2017-10-30 10:35:51 -0700

    Intro cam, nav mesh

--- Assets/Scripts/Enemies/Enemy_Attack.cs
+++ Assets/Scripts/Enemies/Enemy_Attack.cs
@@ -7,24 +7,42 @@ public class Enemy_Attack : MonoBehaviour {
     public int m_damagePerHit; //could be changed to become weapon specific
     public float attackFrequency = 3.0f;
     private float startingFreq;
+	private Animator anim;
 
     private void Awake()
     {
+		anim = GetComponent<Animator> ();
         startingFreq = attackFrequency;
     }
 
+	private void OnCollisionEnter(Collision collision)
+	{
+		if (collision.rigidbody && collision.gameObject.tag == "Player")
+		{
+			anim.SetBool ("Attacking", true);
+		}
+	}
+
     private void OnCollisionStay(Collision collision)
     {
-        if (collision.rigidbody) //if the target of the collision has a rigidbody, try to do damage to it
+		if (collision.rigidbody && collision.gameObject.tag == "Player") //if the target of the collision has a rigidbody, try to do damage to it
         {
             attackFrequency -= Time.deltaTime;
             Debug.Log("Colliding with rigidbody");
             if (attackFrequency <= 0.0f)
             {
+				if (!anim.GetBool ("Attacking"))
+					anim.SetBool ("Attacking", true);
+				
                 Hero_Health heroHealth = collision.collider.GetComponent<Hero_Health>();   // checks to see if the object attacked has a EnemyHealth script
                 heroHealth.TakeDamage(m_damagePerHit);
                 attackFrequency = startingFreq;
             }
         }
     }
+
+	private void OnCollisionExit()
+	{
+		anim.SetBool ("Attacking", false);
+	}
 }
--- Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -11,7 +11,6 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         public ThirdPersonCharacter character { get; private set; } // the character we are controlling
         public Transform target;                                    // target to aim for
 
-
         private void Start()
         {
             // get the components on the object we need ( should not be null due to require component so no need to check )
@@ -21,7 +20,7 @@ namespace UnityStandardAssets.Characters.ThirdPerson
 	        agent.updateRotation = false;
 	        agent.updatePosition = true;
         }
-
+			
 
         private void Update()
         {
@@ -32,12 +31,6 @@ namespace UnityStandardAssets.Characters.ThirdPerson
                 character.Move(agent.desiredVelocity, false, false);
             else
                 character.Move(Vector3.zero, false, false);
-        }
-
-
-        public void SetTarget(Transform target)
-        {
-            this.target = target;
         }
     }
 }
